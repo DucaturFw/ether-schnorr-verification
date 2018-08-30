940ef22c569236fe3612f8ca0fe535c31bc1683a
Aler Denisov <aler.zampillo@gmail.com>
2018-08-30 16:31:46 +0300

    migrate curve lib and optimized schnorr to network

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,4 +1,9 @@
 var Migrations = artifacts.require("./Migrations.sol");
+var Curve = artifacts.require("./Curve.sol");
+var SchnorrOptimized = artifacts.require("./SchnorrOptimized.sol");
 module.exports = function(deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(Curve);
+  deployer.link(Curve, SchnorrOptimized);
+  deployer.deploy(SchnorrOptimized);
 };
